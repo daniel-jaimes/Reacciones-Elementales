379ee7e23a78512a0c5781ef59bc05006ffd925d
Daniel J <dajaca00@gmail.com>
2021-10-17 21:03:27 +0200

    write a good print

--- src/dao/ResultsReader.java
+++ src/dao/ResultsReader.java
@@ -4,8 +4,8 @@ import java.io.FileReader;
 
 public class ResultsReader {
     public FileReader getResults(){
-        System.out.println("SE ESTA LEYENDO EN EL FICHERO...");
-        return new Reader().readerPath("files/resultadoElementales.txt");
+        System.out.println("SE ESTA LEYENDO EN EL FICHERO \"resultadosElementales.txt\"...");
+        return new Reader().readerPath("files/resultadoElemental.txt");
     }
 
 }

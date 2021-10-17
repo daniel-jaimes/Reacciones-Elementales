a7c546e2547951b8a68b7e9f37472055cd3611a0
Daniel J <dajaca00@gmail.com>
2021-10-17 21:03:44 +0200

    write a good print

--- src/dao/ResultsWriter.java
+++ src/dao/ResultsWriter.java
@@ -5,7 +5,7 @@ import java.io.FileWriter;
 
 public class ResultsWriter {
     public FileWriter setResults(){
-        System.out.println("SE ESTA ESCRIBIENDO EN EL FICHERO...");
+        System.out.println("SE ESTA ESCRIBIENDO EN EL FICHERO \"resultadosElementales.txt\"...");
         return new Writer().writerPath("files/resultadoElemental.txt");
     }
 }

7dda6a2bc0533a5a211b5d8edb818c8a0c04c3aa
Daniel J <danieljaimescarrillo@gmail.com>
2021-10-17 20:34:37 +0200

    many things
    - refactor whichCombination function
    - create write function

--- src/manager/ElementsController.java
+++ src/manager/ElementsController.java
@@ -1,14 +1,13 @@
 package manager;
 
 import dao.ElementsReader;
-import dao.ResultsWriter;
 
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 
 public class ElementsController {
-    void init() {
+    void init(FileWriter RW) {
         FileReader ER = new ElementsReader().getElements();
         try{
             int content;
@@ -21,28 +20,36 @@ public class ElementsController {
                     content = ER.read();
                     ch2 = (char) content;
                     combination = whichCombination(ch1, ch2);
-                    write(combination);
                     if(combination.equals("")) ch1 = ch2;
+                    else write(combination, RW);
                 } while (content != -1 && combination.equals(""));
-            } while (content != -1 );
+            } while (content != -1);
             ER.close();
+
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
-    private void write(String combination) {
-        FileWriter RW = new ResultsWriter().setResults();
-
+    private void write(String combination, FileWriter RW) {
+        try{
+            RW.write("\n" + combination);
+            RW.flush();
+        } catch (IOException e){
+            e.printStackTrace();
+        }
     }
     private String whichCombination(char ch1, char ch2) {
-        if(ch1 == 'F' && ch2 == 'A') return "Evaporacion";
-        if(ch1 == 'F' && ch2 == 'H') return "Derretido";
-        if(ch1 == 'F' && ch2 == 'R') return "Sobrecarga";
-        if(ch1 == 'R' && ch2 == 'A') return "Electro-carga";
-        if(ch1 == 'R' && ch2 == 'H') return "Superconductor";
-        if(ch1 == 'A' && ch2 == 'H') return "Congelar";
+        String str = ch1 + "" +  ch2;
+        if(str.equals("FA") || str.equals("AF")) return "Evaporacion";
+        if(str.equals("FH") || str.equals("HF")) return "Derretido";
+        if(str.equals("FR") || str.equals("RF")) return "Sobrecarga";
+        if(str.equals("RA") || str.equals("AR")) return "Electro-carga";
+        if(str.equals("RH") || str.equals("HR")) return "Superconductor";
+        if(str.equals("AH") || str.equals("HA")) return "Congelar";
         if(ch1 == 'V' && (ch2 == 'F' || ch2 == 'R' || ch2 == 'A' || ch2 == 'H')) return "Torbellino";
+        if(ch2 == 'V' && (ch1 == 'F' || ch1 == 'R' || ch1 == 'A' || ch1 == 'H')) return "Torbellino";
         if(ch1 == 'T' && (ch2 == 'F' || ch2 == 'R' || ch2 == 'A' || ch2 == 'H')) return "Cristalizar";
+        if(ch2 == 'T' && (ch1 == 'F' || ch1 == 'R' || ch1 == 'A' || ch1 == 'H')) return "Cristalizar";
         if(ch1 == 'F' && ch2 == 'N') return "Quemadura";
         else return "";
     }

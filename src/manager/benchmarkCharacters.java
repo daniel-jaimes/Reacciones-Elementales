844117ac7a1327c0e9b6d5e3d296cbfebd1a3608
Daniel J <danieljaimescarrillo@gmail.com>
2021-10-17 20:32:07 +0200

    update many things
    - create write function
    - create toString function
    - create orderAlphabetically function
    - improve the elements read control  by each character

--- src/manager/benchmarkCharacters.java
+++ src/manager/benchmarkCharacters.java
@@ -1,5 +1,7 @@
 package manager;
 
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.stream.Collectors;
@@ -15,7 +17,18 @@ public class benchmarkCharacters {
         this.totalCharacters = characters.size();
         //Guradamos todos los elementos del array de personaje
         characters.forEach(character -> {
-            elements.add(String.valueOf(character.getType()));
+            char type = character.getType();
+            switch (type){
+                case 'F':
+                case 'R':
+                case 'A':
+                case 'H':
+                case 'T':
+                case 'V':
+                case 'N':
+                    elements.add(String.valueOf(character.getType()));
+                    break;
+            }
         });
         //Filtramos los elementos que se repiten
         elementsUsed = (ArrayList<String>) elements
@@ -29,19 +42,55 @@ public class benchmarkCharacters {
         elementsUsed.forEach(element -> {
             counterByElement.add(String.valueOf(Collections.frequency(elements, element)));
         });
-        //Le damos valor
-        this.charactersByElements[0] = elementsUsed.toArray(new String[0]);
-        this.charactersByElements[1] = counterByElement.toArray(new String[0]);
-
+        //Order alphabetically and set value
+        this.charactersByElements = orderAlphabetically(elementsUsed, counterByElement);
     }
+
+    private String[][] orderAlphabetically(ArrayList<String> elementsUsed, ArrayList<String> counterByElement) {
+        ArrayList<String> orderedElements = new ArrayList<String>();
+        ArrayList<String> orderedValues = new ArrayList<String>();
+        String[][] charactersByElements = new String[2][elementsUsed.size()];
+        do{
+            int j = 0;
+            char char1 = elementsUsed.get(j).charAt(0);
+            int index1 = j;
+            for (int k = j + 1; k < elementsUsed.size(); k++) {
+                char char2 = elementsUsed.get(k).charAt(0);
+                if (char2 < char1){
+                    char1 = char2;
+                    index1 = k;
+                }
+            }
+            orderedElements.add(elementsUsed.get(index1));
+            elementsUsed.remove(index1);
+            orderedValues.add(counterByElement.get(index1));
+            counterByElement.remove(index1);
+        }while(elementsUsed.size() > 0);
+        charactersByElements[0] = orderedElements.toArray(new String[0]);
+        charactersByElements[1] = orderedValues.toArray(new String[0]);
+        return charactersByElements;
+    }
+
     @Override
     public String toString() {
         String str = "Total de personajes: " + this.totalCharacters +
                 "\nTotal de elementos utilizados: " + this.totalElementsUsed +
                 "\nTotal de personajes por elemento:";
-        for (int i = 0; i < this.charactersByElements.length; i++) {
-            str += "\n\t- " + this.charactersByElements[0][i] + " : " + this.charactersByElements[1][i];
+        for (int i = 0; i < this.charactersByElements[0].length; i++) {
+            if(Integer.parseInt(this.charactersByElements[1][i]) > 0) {
+                str += "\n\t- " + this.charactersByElements[0][i] + " : " + this.charactersByElements[1][i];
+            }
         }
         return str;
     }
+
+    void write(FileWriter RW) {
+        try{
+            RW.write(this.toString());
+            RW.flush();
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
 }

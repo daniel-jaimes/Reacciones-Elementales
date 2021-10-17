7de60ced14ac2a1a75b43b98bf0d1e4df8712bcf
Daniel J <danieljaimescarrillo@gmail.com>
2021-10-17 20:32:26 +0200

    refactor controller class

--- src/manager/Controller.java
+++ src/manager/Controller.java
@@ -1,28 +1,39 @@
 package manager;
 
 import dao.CharacterReader;
+import dao.ResultsReader;
+import dao.ResultsWriter;
 import model.Character;
 
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 
 public class Controller {
     public void init() {
         writeResults(readCharacters());
-        printStatistics();
+        //printStatisticsTerminal();
     }
 
     private void writeResults(ArrayList<Character> characters) {
         benchmarkCharacters bmC = new benchmarkCharacters(characters);
         ElementsController EC = new ElementsController();
-
-        EC.init();
+        FileWriter RW = new ResultsWriter().setResults();
+        bmC.write(RW);
+        EC.init(RW);
+        try {
+            RW.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
     private ArrayList<model.Character> readCharacters() {
         return new CharacterReader().getCharacters();
     }
 
-    private void printStatistics() {
-
+    private void printStatisticsTerminal() {
+        FileReader RR = new ResultsReader().getResults();
     }
 
 }

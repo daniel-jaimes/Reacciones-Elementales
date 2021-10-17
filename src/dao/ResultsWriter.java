163678c05e62887ffb39f8bd272c9dc8b4270bcd
Daniel J <danieljaimescarrillo@gmail.com>
2021-10-17 20:28:50 +0200

    write the correct file name

--- src/dao/ResultsWriter.java
+++ src/dao/ResultsWriter.java
@@ -6,6 +6,6 @@ import java.io.FileWriter;
 public class ResultsWriter {
     public FileWriter setResults(){
         System.out.println("SE ESTA ESCRIBIENDO EN EL FICHERO...");
-        return new Writer().writerPath("files/resultadoElementales.txt");
+        return new Writer().writerPath("files/resultadoElemental.txt");
     }
 }
